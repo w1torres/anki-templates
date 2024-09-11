c9e366fa9b093d9b4ed1b0acec970c90edd3e1bf
Wildes Torres da Silva <wildestorres@gmail.com>
2024-09-11 17:23:46 -0300

    Inserindo Decks dinâmicamente

--- scripts/create_decks.py
+++ scripts/create_decks.py
@@ -15,31 +15,14 @@ def create_deck(deck_name):
 
 def main():
     # Definir a hierarquia dos baralhos e sub-baralhos conforme o padrão solicitado
-    decks = [
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::DESENVOLVIMENTO DE SISTEMAS::Desenvolvimento em Linguagens de programação::Java",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::DESENVOLVIMENTO DE SISTEMAS::Desenvolvimento em Linguagens de programação::JavaEE",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::DESENVOLVIMENTO DE SISTEMAS::Desenvolvimento em Linguagens de programação::JakartaEE",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::DESENVOLVIMENTO DE SISTEMAS::Desenvolvimento em Linguagens de programação::JPA",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::DESENVOLVIMENTO DE SISTEMAS::Desenvolvimento em Linguagens de programação::Javascript",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::DESENVOLVIMENTO DE SISTEMAS::Análise estática de código::Clean code, SonarQube",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::DESENVOLVIMENTO DE SISTEMAS::Arquitetura de software::Interoperabilidade de sistemas",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::DESENVOLVIMENTO DE SISTEMAS::Ambientes de aplicação::Internet, intranet, extranet",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::DESENVOLVIMENTO DE SISTEMAS::Ferramentas de gestão da configuração::GIT",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::BANCO DE DADOS::Modelagem de dados::Conceitual, lógica e física",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::BANCO DE DADOS::Linguagens SQL::SQL, DDL, DML",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::BANCO DE DADOS::SGBDs::Características e operações",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::BANCO DE DADOS::Banco de dados NoSQL::Definições e exemplos",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::BANCO DE DADOS::Técnicas de integração de dados::ETL, ELT",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::GESTÃO E GOVERNANÇA DE TI::Gerenciamento de projetos::Conceitos e áreas de conhecimento",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::GESTÃO E GOVERNANÇA DE TI::Gestão de riscos::Gerenciamento de riscos em TI",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::GESTÃO E GOVERNANÇA DE TI::ITIL v4::Conceitos básicos, disciplinas",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::GESTÃO E GOVERNANÇA DE TI::Governança de TI::COBIT 2019",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::INTELIGÊNCIA DE NEGÓCIOS::Conceitos de BI::Fundamentos e características",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::INTELIGÊNCIA DE NEGÓCIOS::Data warehouse::Arquitetura e aplicações",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::SEGURANÇA DA INFORMAÇÃO::Políticas de segurança::Conceitos e definições",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::SEGURANÇA DA INFORMAÇÃO::Normas e procedimentos::ABNT NBR ISO/IEC 27001 e 27002",
-        "CONHECIMENTOS ESPECÍFICOS - DATAPREV::SEGURANÇA DA INFORMAÇÃO::Mecanismos de segurança::Controle de acesso, OAuth2, SSO"
-    ]
+    decks = []
+
+    # Coletar os nomes dos baralhos do usuário dinamicamente
+    while True:
+        deck_name = input("Digite o nome do baralho (ou 'sair' para finalizar): ")
+        if deck_name.lower() == 'sair':
+            break
+        decks.append(deck_name)
 
     # Criar todos os baralhos e sub-baralhos
     for deck in decks:
